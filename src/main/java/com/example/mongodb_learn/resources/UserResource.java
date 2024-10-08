1ed483e0113010d1cde0611a717730bda0eefdb0
Miqueias Bento <miqueias.bento32@gmail.com>
2024-10-08 16:09:12 -0300

    Entity User e REST

---
+++ src/main/java/com/example/mongodb_learn/resources/UserResource.java
@@ -0,0 +1,28 @@
+package com.example.mongodb_learn.resources;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.mongodb_learn.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@RequestMapping(method = RequestMethod.GET)		// Mapeia uma operacao GET
+	public ResponseEntity<List<User>> findAll() {
+		User user1 = new User("1", "Maria Silva", "mariabrow@gmail.com");
+		User user2 = new User("2", "Alex Maia", "alexgreen@gmail.com");
+		
+		List<User> list = new ArrayList<>();
+		list.addAll(Arrays.asList(user1, user2));
+		
+		return ResponseEntity.ok().body(list);
+	}
+}
