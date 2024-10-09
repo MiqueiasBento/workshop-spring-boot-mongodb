ffae50c28c2642511c97d07e6d958d20c06c8afa
Miqueias Bento <miqueias.bento32@gmail.com>
2024-10-09 10:18:05 -0300

    Atualizacao de usuario com UPDATE

--- src/main/java/com/example/mongodb_learn/services/UserService.java
+++ src/main/java/com/example/mongodb_learn/services/UserService.java
@@ -33,8 +33,20 @@ public class UserService {
 		userRepository.deleteById(id);
 	}
 	
+	public User update(User user) {
+		User newUser = findById(user.getId());
+		updateData(newUser, user);
+		
+		return userRepository.save(newUser);
+	}
+	
+	private void updateData(User newUser, User user) {
+		newUser.setId(user.getId());
+		newUser.setName(user.getName());
+		newUser.setEmail(user.getEmail());
+	}
+
 	public User fromDTO(UserDTO userDto) {
 		return new User(userDto.getId(), userDto.getName(), userDto.getEmail());
 	}
-	
 }
